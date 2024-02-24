473fb64841e399b576f6ac2ec6f45029b2ffb99f
Panda260 <Panda260@NeverStopGaming.net>
2024-02-24 15:10:26 +0100

    init

---
+++ install.sh
@@ -0,0 +1,15 @@
+
+curl -o SimpleCloud-Latest.zip -Li https://github.com/theSimpleCloud/SimpleCloud/releases/download/v2.7.1/SimpleCloud-v2.7.1.zip?raw=true
+curl -o SimpleCloud.zip -Li https://github.com/NeverStopGaming/SimpleCloud-Pterodactyl-egg/blob/main/download/SimpleCloud.zip?raw=true
+
+unzip SimpleCloud-Latest.zip
+unzip SimpleCloud.zip
+
+chmod 777 runner.jar
+chmod 777 launcher.jar
+chmod 777 storage/base.jar
+
+rm -r SimpleCloud.zip
+rm -r SimpleCloud-Latest.zip
+
+echo "SimpleCloud wurde erfolgreich Herruntergeladen!"
